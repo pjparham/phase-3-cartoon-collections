7744d08735b3c0d53b1805b55f119bf811245ba4
Parker Parham <parkerparham@gmail.com>
2022-10-14 14:18:50 -0400

    done with assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(names)
+  names.each.with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+ calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_array = cheese_types.map do |cheese|
+    if snacks.include?(cheese)
+      cheese
+    end
+  end
+  if cheese_array.filter {|element| element != nil}.join == ""
+    nil
+  else
+    cheese_array.filter {|element| element != nil}.join
+  end
+  # cheese_array.filter {|element| element != nil}.join
 end
+
